f21fb44794970595378b02071bfe07ca5505cc18
MuradShaw <60895889+MuradShaw@users.noreply.github.com>
2021-09-04 16:13:01 -0400

    jump(ing for joy)

--- WPTM/Assets/Scripts/game/gm_movementHandler.cs
+++ WPTM/Assets/Scripts/game/gm_movementHandler.cs
@@ -7,49 +7,75 @@ public class gm_movementHandler : MonoBehaviour
     bool moveLeft;
     bool moveRight;
     bool jumping;
+    bool activeJumping;
+    bool shortHop;
     bool run;
+    bool isGrounded;
     int jumps;
     int defaultJumps;
 
-    //Crucial player stats:
+    //Character stats
     float maxSpeed;
     float runTimeZeroToMax; //accel
     float runTimeMaxToZero; //dccel
     float accel;
     float dccel;
     float fallspeed;
+    float airAccel; //Air accel
+    float airDccel;
+    float airDrift; //Max air speed
 
     float leftVelocity;
     float rightVelocity;
+
+    float jumpVelocity;
+    float fullhopMax;
+    float shorthopMax;
+    float jumpTimeZeroToMax; //jcell
+    float jccel;
+
+    bool doubleJump;
     int neg;
 
     Rigidbody rb; 
 
-    public void OnTriggerEnter(Collider other)
+    /*public void OnCollisionEnter(Collider other)
     {
         //if(other.tag == "JumpRestoration" && jumps < defaultJumps) restoreJumps();
-    }
+    } */
 
     void Start()
     {
         moveLeft = false;
         moveRight = false;
+        activeJumping = false;
         run = false;
 
+        doubleJump = false;
+
         maxSpeed = 20f;
+        airDrift = 2.5f;
+        fullhopMax = 15f;
+        shorthopMax = 9.5f;
+        jumpTimeZeroToMax = 0.2f;
         runTimeZeroToMax = 0.2f;
         runTimeMaxToZero = 0.3f;
 
         accel = maxSpeed / runTimeZeroToMax;
         dccel = -maxSpeed / runTimeMaxToZero;
+        airAccel = airDrift / runTimeZeroToMax;
+        airDccel = -airDrift / runTimeMaxToZero;
+
         leftVelocity = 0f;
         rightVelocity = 0f;
+        jumpVelocity = 0.0f;
         neg = 1;
 
-        fallspeed = 12.0f;
+        isGrounded = true;
+        fallspeed = 45.0f;
         defaultJumps = 1;
-
         jumps = defaultJumps;
+
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
     }
@@ -60,47 +86,115 @@ public class gm_movementHandler : MonoBehaviour
             moveLeft = true;
         if(Input.GetKey(KeyCode.D))
             moveRight = true;
+        //if(Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.M) && jumping == true) 
+        //    doubleJump = true;
         if(Input.GetKey(KeyCode.Space))
             jumping = true;
-    }
-
-    void FixedUpdate()
-    {
-        if(moveRight) neg = -1; else if(moveLeft) neg = 1;
-
-        /* Move Left */
-        if(moveLeft || moveRight)
-        {
-            leftVelocity += accel * Time.deltaTime;
-            leftVelocity = Mathf.Min(leftVelocity, maxSpeed); //are we max speed?
-
-            rb.velocity = Vector3.left * leftVelocity * neg; 
-        }
-        else //dcell from lack of movement
-        {
-            leftVelocity += dccel * Time.deltaTime;
-            leftVelocity = Mathf.Max(leftVelocity, 0);
-
-            rb.velocity = Vector3.left * leftVelocity * neg; 
-        }
+        if(Input.GetKey(KeyCode.M))
+            shortHop = true;
 
         if(moveLeft && !Input.GetKey(KeyCode.A))
             moveLeft = false;
         if(moveRight && !Input.GetKey(KeyCode.D))
             moveRight = false;
+    }
+    
+    void FixedUpdate()
+    {
+        //are we grounded type check
+        isGrounded = groundCheck();
+        jumps = (groundCheck()) ? defaultJumps : jumps;
+
+        if(moveRight) neg = -1; else if(moveLeft) neg = 1;
+
+        bool wallShinanigans = wallCheck(Vector3.left * neg);
+
+        Debug.Log(wallShinanigans);
+
+        if((moveLeft || moveRight) && !wallShinanigans)
+            accelerate(((!isGrounded) ? airAccel : accel), maxSpeed, false, neg);
+        else
+            accelerate(((!isGrounded) ? airDccel : dccel), maxSpeed, true, neg);
+        
+        //Double check to avoid rocket launching into a wall and ruining the dinner
+        if(wallShinanigans) accelerate(dccel*5, maxSpeed, true, neg);
 
         updateGravity();
+
+        if((jumping || shortHop) && jumps > 0)
+            jump(shortHop);
     }
 
-    // Maybe soon <3
-    void Accel(float accel)
+    //for all your movement wants and needs
+    void accelerate(float a, float max, bool d, int n)
     {
-        return;
+        //Debug.Log("Airborne accel: " + isGrounded);
+
+        if(!d) //Accelerating
+        {
+            leftVelocity += a * Time.deltaTime;
+            leftVelocity = Mathf.Min(leftVelocity, max); //are we max speed?
+
+            rb.velocity = Vector3.left * leftVelocity * n; 
+        }
+        else //Deccelerating (from lack of schmovement)
+        {
+            leftVelocity += a * Time.deltaTime;
+            leftVelocity = Mathf.Max(leftVelocity, 0);
+
+            rb.velocity = Vector3.left * leftVelocity * n; 
+        }
     }
 
+    //for all of your short and full hopping needs
+    void jump(bool shorthop = false)
+    {
+        if(isGrounded) accelerate(dccel * 3f, maxSpeed, true, neg);
+
+        jccel = ((shorthop) ? shorthopMax : fullhopMax) / jumpTimeZeroToMax; //Calculate jump accel using short/full hop values
+
+        //Clean up the mess
+        if(jumpVelocity >= (((shorthop) ? shorthopMax : fullhopMax)))
+        {
+            jumpVelocity = 0.0f; //my physics teacher would be proud
+            
+            jumps--;
+            jumping = false;
+            shortHop = false;
+        }
+
+        //preform the jump!! 
+        jumpVelocity += jccel * Time.deltaTime;
+        rb.AddForce(new Vector3(0, jumpVelocity, 0), ForceMode.Impulse); //physics!!!!
+
+        isGrounded = false;
+    }
+
+    //updating individual gravity for characters
     void updateGravity()
     {
-        Vector3 gravity = -35.81f * fallspeed * Vector3.up;
+        Vector3 gravity = -9.81f * fallspeed * Vector3.up;
         rb.AddForce(gravity, ForceMode.Acceleration);
+    }
+
+    //are we grounded?
+    bool groundCheck()
+    {
+        RaycastHit hit;
+	    float distance = 0.7f;
+	    Vector3 dir = new Vector3(0, -1);
+
+        //See if we're grounded via raycast
+	    return(Physics.Raycast(transform.position, dir, out hit, distance));
+    }
+
+    //stop clipping on walls dammit
+    bool wallCheck(Vector3 dir)
+    {
+        RaycastHit hit;
+	    float distance = 0.7f;
+
+        //See if we're grounded via raycast
+	    return(Physics.Raycast(transform.position, dir, out hit, distance));
     }
 }
